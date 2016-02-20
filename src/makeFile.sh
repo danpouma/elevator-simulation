5a562583e26c86969477b2bedc646a12b5a8abde
danpouma <dpoumakis@yahoo.com>
2016-02-19 19:46:22 -0500

    added make file to compile, run and store test results

---
+++ src/makeFile.sh
@@ -0,0 +1,9 @@
+# !/bin/sh
+
+java elevatorTest.ClockTest >> ClockTestResults.txt
+java elevatorTest.ElevatorButtonTest >> ElevatorButtonTestResults.txt
+java elevatorTest.ElevatorTest >> ElevatorTestResults.txt
+java elevatorTest.FloorArrayTest >> FloorArrayTestResults.txt
+java elevatorTest.FloorQueueTest >> FloorQueueTestResults.txt
+java elevatorTest.FloorTest >> FloorTestResults.txt
+java elevatorTest.PersonTest >> PersonTestResults.txt

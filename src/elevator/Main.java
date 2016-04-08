02e34425cc461efe37e29d161be3da45e96eafa5
danpouma <dpoumakis@yahoo.com>
2016-04-07 21:38:25 -0400

    update spec

--- src/elevator/Main.java
+++ src/elevator/Main.java
@@ -8,6 +8,9 @@ public class Main
 {
     public static void main(String[] args)
     {
+        // Have entire setup in the config moved to simulation class
+        // not good programming to have it in there.
+        
         // Simulation loop is kicked off in gui
         ConfigGui simulation = new ConfigGui();
     }
